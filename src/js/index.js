4713b3f56421b55d9f2ffe3b031e8c40999fe762
Alex Bond <bondarenko.aleksandr.25@gmail.com>
2023-04-05 16:48:42 +0600

    maked animate for section out-mission

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
     const cookie = document.querySelector('.cookie');
     const button = cookie.querySelector('.cookie__btn');
-
-    const animateImage = document.querySelector('.our-mission__img')
+    const blckOurMission = document.querySelector('.our-mission');
+    const animateImage = document.querySelector('.our-mission__img');
+    const animateText = document.querySelector('.our-mission__text');
 
     //view cookie
     const timerId = setTimeout(() => {
@@ -14,7 +15,26 @@ window.addEventListener('DOMContentLoaded', () => {
         cookie.style.transform = `translateY(18rem)`;
     });
 
-    //animte block out-mission
+    //animate block out-mission
+    if (window.matchMedia("(min-width: 376px)").matches) {
+        const blckOurMissionObserver = new IntersectionObserver((entries, observer) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    animateImage.style.left = '50%';
+                    animateImage.style.transform = `translateX(-50%) rotate(90deg)`;
+                    animateText.style.transform = `translateX(50rem)`;
+                    animateText.style.opacity = '0';
+                    const timerID = setTimeout(() => {
+                        animateImage.style.transform = `translateX(-50%) rotate(0deg) scale(0.7)`;
+                        clearTimeout(timerID)
+                    }, 3000)
+                    observer.unobserve(blckOurMission)
+                }
+            })
+        }, { threshold: [0.5, 0.7] })
 
-    animateImage.style.transform = `translateX(0rem) rotate(90deg)`;
+        blckOurMissionObserver.observe(blckOurMission)
+    }
+
+
 });
